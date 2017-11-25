cfa71b55b1e22529100791c636a8426ada2e9d45
PixelwarStudio <janko.matthes@gmx.net>
2017-11-25 13:42:29 +0100

    add more functionality

--- PillowTurtle.py
+++ PillowTurtle.py
@@ -6,28 +6,29 @@ import attr
 class Turtle(object):
     pos = attr.ib(default=[0, 0])
     rot = attr.ib(default=0)
-    visible = attr.ib(default=True)
+    _visible = attr.ib(default=True)
+    type = attr.ib(default="line")
     width = attr.ib(default=1)
     color = attr.ib(default=(255, 255, 255))
 
     def __attrs_post_init__(self):
         self._path = [] 
         self._stack = []
+        self._changed = False
 
     def move(self, dist):
-        if self.visible:
-            if self._path == [] or self._path[len(self._path)-1] is False:
-                self._path.append([(self.x, self.y)])
-        else:
-            self._path.append(False)
+        if self._visible:
+            if self._path == [] or self._changed or self._props_changed():
+                self._path.append([self.type, self.width, self.color, (self.x, self.y)])
+                self._changed = False
                 
         # Calculate new position
         rad_rot = rad(self.rot)
         self.x += cos(rad_rot) * dist
         self.y += sin(rad_rot) * dist
 
-        if self.visible:
-            self._path.append([(self.x, self.y), self.width, self.color])
+        if self._visible:
+            self._path[len(self._path)-1].append((self.x, self.y))
 
     def rotate(self, degrees):
         self.rot += degrees
@@ -37,25 +38,47 @@ class Turtle(object):
         self._stack.append(self.rot)
 
     def pop(self):
-        prev_visible = self.visible
+        prev_visible = self._visible
 
-        self.visible = False
+        self._visible = False
 
         self.rot = self._stack.pop()
         self.pos = self._stack.pop()
 
-        self.visible = prev_visible
+        self._visible = prev_visible
     
     def draw(self, canvas):
         draw = ImageDraw.Draw(canvas)
-        for n in range(1, len(self._path)):
-            if self._path[n] is False or self._path[n-1] is False:
-                continue
+        for segment in self._path:
+            type = segment[0]
+            width = segment[1]
+            color = segment[2]
 
-            p1, p2 = self._path[n-1][0], self._path[n][0]
-            color, width = self._path[n][2], self._path[n][1]
+            if type=="line":
+                draw.line(segment[3:], color, width)
+            elif type=="polygon":
+                draw.polygon(segment[3:], color)
+    
+    def _props_changed(self):
+        if self._path != []:
+            curr_segment = self._path[len(self._path)-1]
 
-            draw.line(p1+p2, color, width)
+            typ = curr_segment[0]
+            width = curr_segment[1]
+            color = curr_segment[2]
+
+            return typ != self.type or width != self.width or color != self.color
+        return False
+
+    @property
+    def visible(self):
+        return self._visible
+
+    @visible.setter
+    def visible(self, x):
+        if x is False and self._visible is True:
+            self._changed = True
+        self._visible = x
 
     @property
     def x(self):
