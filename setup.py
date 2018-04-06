2e8ae328d65e9e824e20a8f8edb291354051df48
PixelwarStudio <janko.matthes@gmx.net>
2018-04-06 18:38:36 +0200

    add basic setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 from setuptools import setup
 
 setup(
-    name="PillowTurtle"
+    name="PillowTurtle",
+    version="0.1",
+    description="",
+    url="",
+    author="Pixelwar",
+    author_email="code@pxlwr.de",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+    ],
+    license="MIT",
+    py_modules=["PillowTurtle"],
+    zip_safe=False
 )
